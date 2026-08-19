579bd9589cd53b72f20665eb91e66c1262bf1b26
NeverBackPing <neverbackping@gmail.com>
2026-08-19 04:15:38 +0200

    start linker et bootloader

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,16 @@
 #![no_std]
 #![no_main]
 
+use core::arch::global_asm;
+use core::panic::PanicInfo;
+
+global_asm!(include_str!("boot.S"), options(raw));
+
 #[unsafe(no_mangle)]
 pub extern "C" fn _start() -> ! {
     #[allow(clippy::empty_loop)]
     loop {}
 }
-
-use core::panic::PanicInfo;
 
 #[panic_handler]
 fn panic(_info: &PanicInfo) -> ! {
