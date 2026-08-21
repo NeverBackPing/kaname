474de72208ba984cf72a10b8e405d333245003f6
Kurobyte <me@kurobyte.dev>
2026-08-21 15:45:20 +0200

    feat(vga): add cursor functions and proper initialization

--- src/drivers/vga.rs
+++ src/drivers/vga.rs
@@ -2,6 +2,8 @@ use core::cell::UnsafeCell;
 use core::fmt;
 use core::ptr::{read_volatile, write_volatile};
 
+use crate::ports;
+
 const VGA_BUFFER: *mut u16 = 0xb8000 as *mut u16;
 const WIDTH: usize = 80;
 const HEIGHT: usize = 25;
@@ -10,14 +12,36 @@ const HEIGHT: usize = 25;
 #[derive(Clone, Copy)]
 #[allow(dead_code)]
 pub enum Color {
-    Black = 0, Blue = 1, Green = 2, Cyan = 3, Red = 4, Magenta = 5,
-    Brown = 6, LightGrey = 7, DarkGrey = 8, LightBlue = 9, LightGreen = 10,
-    LightCyan = 11, LightRed = 12, LightMagenta = 13, Yellow = 14, White = 15,
+    Black = 0,
+    Blue = 1,
+    Green = 2,
+    Cyan = 3,
+    Red = 4,
+    Magenta = 5,
+    Brown = 6,
+    LightGrey = 7,
+    DarkGrey = 8,
+    LightBlue = 9,
+    LightGreen = 10,
+    LightCyan = 11,
+    LightRed = 12,
+    LightMagenta = 13,
+    Yellow = 14,
+    White = 15,
 }
 
 fn entry(c: u8, fg: Color, bg: Color) -> u16 {
     let attr = (fg as u8) | ((bg as u8) << 4);
     (c as u16) | ((attr as u16) << 8)
+}
+
+pub fn init() -> () {
+    let terminal = TERMINAL.0.get();
+    unsafe {
+        (*terminal).clear_screen(Color::Black);
+        (*terminal).enable_cursor(14, 15);
+        (*terminal).update_cursor();
+    };
 }
 
 pub struct Writer {
@@ -29,13 +53,54 @@ pub struct Writer {
 
 impl Writer {
     const fn new() -> Self {
-        Writer { row: 0, col: 0, fg: Color::LightGreen, bg: Color::Black }
+        Writer {
+            row: 0,
+            col: 0,
+            fg: Color::LightGreen,
+            bg: Color::Black,
+        }
     }
 
-    fn put_at(&self, row: usize, col: usize, c: u8) {
+    fn put_at(&mut self, row: usize, col: usize, c: u8) {
         unsafe {
-            write_volatile(VGA_BUFFER.add(row * WIDTH + col), entry(c, self.fg, self.bg));
+            write_volatile(
+                VGA_BUFFER.add(row * WIDTH + col),
+                entry(c, self.fg, self.bg),
+            );
         }
+        self.update_cursor();
+    }
+
+    pub fn clear_screen(&mut self, color: Color) {
+        self.bg = color;
+        for y in 0..HEIGHT {
+            for x in 0..WIDTH {
+                unsafe {
+                    write_volatile(VGA_BUFFER.add(y * WIDTH + x), entry(b' ', self.fg, self.bg));
+                }
+            }
+        }
+    }
+    
+    pub fn enable_cursor(&mut self, start: u8, end: u8) {
+        ports::outb(0x3D4, 0x0A);
+        ports::outb(0x3D5, (ports::inb(0x3D5) & 0xC0) | start);
+        ports::outb(0x3D4, 0x0B);
+        ports::outb(0x3D5, (ports::inb(0x3D5) & 0xE0) | end);
+    }
+
+    #[allow(dead_code)]
+    pub fn disable_cursor(&mut self) {
+        ports::outb(0x3D4, 0x0A);
+        ports::outb(0x3D5, 0x20);
+    }
+
+    pub fn update_cursor(&mut self) {
+        let pos: u16 = (self.row * WIDTH + self.col) as u16;
+        ports::outb(0x3D4, 0x0F);
+        ports::outb(0x3D5, pos as u8);
+        ports::outb(0x3D4, 0x0E);
+        ports::outb(0x3D5, (pos >> 8) as u8);
     }
 
     fn scroll(&mut self) {
@@ -53,16 +118,24 @@ impl Writer {
 
     fn newline(&mut self) {
         self.col = 0;
-        if self.row + 1 >= HEIGHT { self.scroll() } else { self.row += 1 }
+        if self.row + 1 >= HEIGHT {
+            self.scroll();
+        } else {
+            self.row += 1;
+        }
+        self.update_cursor();
     }
 
     pub fn write_byte(&mut self, b: u8) {
         match b {
             b'\n' => self.newline(),
             byte => {
-                if self.col >= WIDTH { self.newline() }
+                if self.col >= WIDTH {
+                    self.newline()
+                }
                 self.put_at(self.row, self.col, byte);
                 self.col += 1;
+                self.update_cursor();
             }
         }
     }
--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,35 @@
 #![no_std]
 #![no_main]
 
+use core::arch::asm;
 use core::arch::global_asm;
 use core::panic::PanicInfo;
 
 global_asm!(include_str!("boot.S"), options(raw));
 
+mod drivers;
+mod libk;
+mod ports;
+
 #[unsafe(no_mangle)]
 pub extern "C" fn _start() -> ! {
+    drivers::vga::init();
+    println!("42");
+
     #[allow(clippy::empty_loop)]
-    loop {}
+    loop {
+        unsafe {
+            asm!("cli; hlt");
+        }
+    }
 }
 
 #[panic_handler]
 fn panic(_info: &PanicInfo) -> ! {
     #[allow(clippy::empty_loop)]
-    loop {}
+    loop {
+        unsafe {
+            asm!("cli; hlt");
+        }
+    }
 }
