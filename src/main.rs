780096f475f77d4868d294ee8119fee128625c46
NeverBackPing <neverbackping@gmail.com>
2026-08-27 11:34:10 +0200

    feat(shell command) - implementation shell can run two command reboot et halt

--- src/main.rs
+++ src/main.rs
@@ -74,11 +74,13 @@ pub extern "C" fn kernel_main() -> ! {
     idt::init();
     pic::init();
     keyboard::init();
+    shell::init();
 
     print_boot_screen();
 
     idt::enable_interrupts();
     loop {
+        shell::handle_keyboard();
         if let Some(event) = keyboard::get_key() {
             if let Key::Function(fn_key) = event.key {
                 drivers::vga::switch_terminal(fn_key);
--- src/shell.rs
+++ src/shell.rs
@@ -1,7 +1,131 @@
 use core::arch::asm;
+use crate::drivers::vga;
+use crate::drivers::keyboard::{self, Key, KeyEvent};
 
 use crate::ports::{inb, outb};
 use crate::println;
+
+const MAX_COMMAND: usize = 64;
+
+pub struct Shell {
+    buffer: [u8; MAX_COMMAND],
+    length: usize,
+}
+
+impl Shell {
+    pub const fn new() -> Self {
+        Self {
+            buffer: [0; MAX_COMMAND],
+            length: 0,
+        }
+    }
+
+    pub fn init(&mut self) {
+        vga::putc(b'>');
+        vga::putc(b' ');
+    }
+
+    pub fn handle_key(&mut self, event: KeyEvent) {
+        match event.key {
+            Key::Char(b'\n') => {
+                self.enter();
+            }
+
+            Key::Char(0x08) => {
+                self.backspace();
+            }
+
+            Key::Char(c) => {
+                self.input_char(c);
+            }
+
+            Key::Function(0) => {
+                vga::switch_terminal(0);
+            }
+
+            Key::Function(1) => {
+                vga::switch_terminal(1);
+            }
+
+            Key::Function(2) => {
+                vga::switch_terminal(2);
+            }
+
+            Key::Function(3) => {
+                vga::switch_terminal(3);
+            }
+
+            Key::Function(4) => {
+                vga::switch_terminal(4);
+            }
+
+            Key::Function(5) => {
+                vga::switch_terminal(5);
+            }
+
+            _ => {}
+        }
+    }
+
+    pub fn input_char(&mut self, c: u8) {
+        if self.length >= MAX_COMMAND {
+            return;
+        }
+
+        self.buffer[self.length] = c;
+        self.length += 1;
+
+        vga::putc(c);
+    }
+
+    pub fn backspace(&mut self) {
+        if self.length == 0 {
+            return;
+        }
+
+        self.length -= 1;
+        self.buffer[self.length] = 0;
+
+        vga::backspace();
+    }
+
+    pub fn enter(&mut self) {
+        vga::putc(b'\n');
+
+        if let Ok(command) = core::str::from_utf8(&self.buffer[..self.length]) {
+            execute_cmd(command);
+        }
+
+        self.clear();
+
+        vga::putc(b'>');
+        vga::putc(b' ');
+    }
+
+    fn clear(&mut self) {
+        self.buffer = [0; MAX_COMMAND];
+        self.length = 0;
+    }
+}
+
+static mut SHELL: Shell = Shell::new();
+
+pub fn init() {
+    unsafe {
+        (&raw mut SHELL).as_mut().unwrap().init();
+    }
+}
+
+pub fn handle_keyboard() {
+    while let Some(event) = keyboard::get_key() {
+        unsafe {
+            (&raw mut SHELL)
+                .as_mut()
+                .unwrap()
+                .handle_key(event);
+        }
+    }
+}
 
 #[allow(dead_code)]
 pub fn command_halt() {
@@ -24,5 +148,17 @@ pub fn command_reboot() {
         loop {
             asm!("cli; hlt");
         }
+    }
+}
+
+
+pub fn execute_cmd(command: &str) {
+    match command {
+        "halt" => command_halt(),
+        "reboot" => command_reboot(),
+        "stack" => {}
+        "" => {}
+
+        _ => println!("Unknown command: {}", command),
     }
 }
