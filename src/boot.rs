d3ee96bb8bcba045f42cc7543635c1d19220cf54
Kurobyte <me@kurobyte.dev>
2026-09-01 18:45:04 +0200

    feat: modify boot.rs to request multiboot2 information

--- src/boot.rs
+++ src/boot.rs
@@ -1,24 +1,57 @@
 use core::arch::naked_asm;
 
-const MAGIC: u32 = 0x1BADB002; // Multiboot magic
-const ALIGN: u32 = 1 << 0; // Align loaded modules on page boundaries
-const MEMINFO: u32 = 1 << 1; // Request memory information from GRUB
-const FLAGS: u32 = ALIGN | MEMINFO;
-const CHECKSUM: u32 = (0u32).wrapping_sub(MAGIC.wrapping_add(FLAGS));
+const MAGIC: u32 = 0xE85250D6; // Multiboot2 magic
+const ARCH_I386: u32 = 0;
 
-#[repr(C)]
-struct MultibootHeader {
+const FLAGS_NONE: u16 = 0;
+const FLAG_OPTIONAL: u16 = 1;
+
+const TYPE_END_TAG: u16 = 0;
+const TYPE_INFO_REQ: u16 = 1;
+
+#[repr(C, packed)]
+struct InformationRequestTag {
+    type_: u16,
+    flags: u16,
+    size: u32,
+}
+
+#[repr(C, packed)]
+struct EndTag {
+    type_: u16,
+    flags: u16,
+    size: u32,
+}
+
+#[repr(C, packed)]
+struct Multiboot2Header {
     magic: u32,
-    flags: u32,
+    architecture: u32,
+    header_length: u32,
     checksum: u32,
+    info_req: InformationRequestTag,
+    end: EndTag,
 }
 
 #[unsafe(link_section = ".multiboot")]
 #[used]
-static MULTIBOOT_HEADER: MultibootHeader = MultibootHeader {
+static MULTIBOOT_HEADER: Multiboot2Header = Multiboot2Header {
     magic: MAGIC,
-    flags: FLAGS,
-    checksum: CHECKSUM,
+    architecture: ARCH_I386,
+    header_length: size_of::<Multiboot2Header>() as u32,
+    checksum: 0u32.wrapping_sub(
+        MAGIC.wrapping_add(ARCH_I386.wrapping_add(size_of::<Multiboot2Header>() as u32)),
+    ),
+    info_req: InformationRequestTag {
+        type_: TYPE_INFO_REQ,
+        flags: FLAGS_NONE,
+        size: size_of::<InformationRequestTag>() as u32,
+    },
+    end: EndTag {
+        type_: TYPE_END_TAG,
+        flags: FLAGS_NONE,
+        size: size_of::<EndTag>() as u32,
+    },
 };
 
 pub const STACK_SIZE: usize = 16 * 1024;
@@ -35,6 +68,13 @@ pub extern "C" fn _start() -> ! {
     naked_asm!(
         "lea esp, [{stack} + {stack_size}]",
         "xor ebp, ebp",
+        // reset EFLAGS
+        "push 0",
+        "popf",
+        // Push multiboot information structure pointer
+        "push ebx",
+        // Push magic value
+        "push eax",
         "call {entry}",
         "2:",
         "hlt",
